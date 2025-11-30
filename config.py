7acf8f3b36619d78e61abe3686f2f9ad9a531bf7
namanjasoria41 <70752380+namanjasoria41@users.noreply.github.com>
2025-11-30 18:27:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///certificates.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     TEMPLATE_FOLDER = os.path.join('static', 'templates')
     GENERATED_FOLDER = os.path.join('static', 'generated')
     
+
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
